1ca9fdb79ca0f4c9ff4aec41ebfeb980714a4cd0
ewansou <ewansou@hotmail.com>
2025-05-04 17:37:44 +0800

    fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { Toaster } from "@/components/ui/sonner";
 
-export const inter = Inter({ subsets: ["latin"], variable: "--font-inter" });
+const inter = Inter({ subsets: ["latin"], variable: "--font-inter" });
 
 export const metadata: Metadata = {
   title: "Outward Bound Singapore",
