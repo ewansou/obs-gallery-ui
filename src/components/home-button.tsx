a75f39f2ccc4ee8ad4fd22e6fb44a31c321f9493
ewansou <ewansou@hotmail.com>
2025-05-05 21:35:38 +0800

    change to lougout icon

--- src/components/home-button.tsx
+++ src/components/home-button.tsx
@@ -2,28 +2,28 @@
 
 import React from "react";
 import { Button } from "@/components/ui/button";
-import { HomeIcon } from "lucide-react";
+import { LogOut } from "lucide-react";
 import { ROUTES } from "@/lib/routes";
 import { useRouter } from "next/navigation";
 import { deleteAuthCredentials } from "@/lib/auth-utils";
 
 const HomeButton = () => {
     const router = useRouter();
-
+  
     return (
-        <div>
-            <Button
-                className="h-14 px-8 text-base min-w-[130px]"
-                onClick={() => {
-                    deleteAuthCredentials();
-                    router.replace(ROUTES.LOGIN);
-                }}
-            >
-                <HomeIcon className="w-6 h-6 mr-3" />
-                Back
-            </Button>
-        </div>
+      <div>
+        <Button
+          className="h-13 px-10 text-lg font-semibold min-w-[140px] gap-3"
+          onClick={() => {
+            deleteAuthCredentials();
+            router.replace(ROUTES.LOGIN);
+          }}
+        >
+          <LogOut className="w-7 h-7" />
+          Exit
+        </Button>
+      </div>
     );
-};
-
-export default HomeButton;
+  };
+  
+  export default HomeButton;
