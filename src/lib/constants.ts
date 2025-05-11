90e701c3da933eba824fa4fde3848cf555e1d52f
ewansou <ewansou@hotmail.com>
2025-05-11 09:17:58 +0800

    gitingore

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -2,4 +2,7 @@ export const AUTH_CRED = "session";
 
 export const revalidate = 30;
 
-export const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL!;
+export const BASE_URL =
+  process.env.NODE_ENV === "development"
+    ? process.env.NEXT_PUBLIC_BASE_URL!
+    : "https://server.highlightsfromyourevent.com/instantly-obs-webapp-service/api";
